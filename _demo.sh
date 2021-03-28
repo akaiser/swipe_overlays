53cb67b3353b144ecf0756251ba2323bcc3ee7dc
Albert Kaiser <ak@kaiserv.de>
2021-03-28 21:35:16 +0200

    renderer demo split

--- _demo.sh
+++ _demo.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+rm -r _demo/canvaskit/*
+rm -r _demo/html/*
+
 flutter clean
-flutter build web --web-renderer canvaskit --release
-#flutter build web --web-renderer html --release
+flutter build web --web-renderer canvaskit
+cp -r build/web/* _demo/canvaskit
+sed -i 's|\="/"|\="/swipe_overlays/canvaskit/"|' _demo/canvaskit/index.html
 
-rm -r _demo/*
-cp -r build/web/* _demo/
-
-sed -i 's|\="/"|\="/swipe_overlays/"|' _demo/index.html
+flutter clean
+flutter build web --web-renderer html
+cp -r build/web/* _demo/html
+sed -i 's|\="/"|\="/swipe_overlays/html/"|' _demo/html/index.html
 
 # git subtree push --prefix _demo origin gh-pages
