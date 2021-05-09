8c5e7aced973347d1e93a86a32f6a20571b52082
akaiser <ak@kaiserv.de>
2021-05-09 23:11:49 +0200

    enable android builds

--- _demo.sh
+++ _demo.sh
@@ -2,15 +2,17 @@
 
 rm -r _demo/canvaskit/*
 rm -r _demo/html/*
-
 flutter clean
+
 flutter build web --web-renderer canvaskit
 cp -r build/web/* _demo/canvaskit
 sed -i "" 's|\="/"|\="/swipe_overlays/canvaskit/"|' _demo/canvaskit/index.html
 
-flutter clean
 flutter build web --web-renderer html
 cp -r build/web/* _demo/html
 sed -i "" 's|\="/"|\="/swipe_overlays/html/"|' _demo/html/index.html
+
+flutter build apk --split-per-abi
+cp build/app/outputs/apk/release/app-* _demo/apk/
 
 # git subtree push --prefix _demo origin gh-pages
